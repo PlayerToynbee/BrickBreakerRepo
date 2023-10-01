ebdb5ea7bb0eaaee888a073838bf87599de74b6f
Jayden <Forcegems@gmail.com>
2023-09-30 23:06:56 -0600

    Material

---
+++ Assets/IntData.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewIntData", menuName = "ScriptableObjects/IntData", order = 1)]
+public class IntData : ScriptableObject
+{
+    public int value;
+}
